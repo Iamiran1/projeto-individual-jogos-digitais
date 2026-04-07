6a91f6c436c7e6899ba27c976d3e9207bf84052f
Iamiran1 <vinicius.c.miranda@icloud.com>
2026-04-07 17:28:36 -0300

    Jogo base só com tutorial

---
+++ Primeiro Micro Game/Assets/_Recovery/MenuActions.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuActions : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void IniciaJogo()
+    {
+        GameController.Init();
+        SceneManager.LoadScene(1);
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
