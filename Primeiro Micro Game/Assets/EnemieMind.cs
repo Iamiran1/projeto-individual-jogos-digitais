8226196e90e8d97e0cbdd0f55703eadb85110be5
Iamiran1 <vinicius.c.miranda@icloud.com>
2026-04-09 18:32:52 -0300

    adicionando função de tocar sons

--- Primeiro Micro Game/Assets/EnemieMind.cs
+++ Primeiro Micro Game/Assets/EnemieMind.cs
@@ -28,3 +28,4 @@ public class AiMind : MonoBehaviour
         }
     }
 }
+
--- Primeiro Micro Game/Assets/PlayerMovement.cs
+++ Primeiro Micro Game/Assets/PlayerMovement.cs
@@ -3,13 +3,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private AudioSource audioSource;
 
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpForce = 8f;
 
+    [Header("Sons")]
+    [SerializeField] private AudioClip somPulo;
+    [SerializeField] private AudioClip somColetavel;
+    [SerializeField] private AudioClip somDano;
+    [SerializeField] private AudioClip somMorte;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -17,6 +25,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            TocarSom(somPulo);
         }
     }
 
@@ -26,13 +35,22 @@ public class PlayerController : MonoBehaviour
         rb.linearVelocity = new Vector2(moveHorizontal * speed, rb.linearVelocity.y);
     }
 
+    private void TocarSom(AudioClip clip)
+    {
+        if (clip != null)
+            audioSource.PlayOneShot(clip);
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Coletavel"))
         {
-            GetComponent<AudioSource>().Play();
+            TocarSom(somColetavel);
             GameController.Collect();
             Destroy(other.gameObject);
         }
     }
+
+    public void TocarSomDano() => TocarSom(somDano);
+    public void TocarSomMorte() => TocarSom(somMorte);
 }
